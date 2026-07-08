f5e64ec33ec16238ec4aa1b205b897a796c93f4b
Yueyan Tang <77295666+yyt005@users.noreply.github.com>
2026-07-07 23:28:00 -0700

    updated consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '乐言的博客';
-export const SITE_DESCRIPTION = '欢迎来我的空间踩踩';
+export const SITE_TITLE = "乐言的空间 · Yueyan's Space";
+export const SITE_DESCRIPTION = "欢迎来我的空间踩踩 · Welcome to my space";
